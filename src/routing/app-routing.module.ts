8486ade86f63d9a466589a6f0d2256061d374ed0
Shree Gotteti <gotteti.2@wright.edu>
2022-08-11 14:49:47 -0400

    update routing

--- src/routing/app-routing.module.ts
+++ src/routing/app-routing.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from 'src/components/about/about.component';
+import { UserDetailComponent } from 'src/components/user-detail/user-detail.component';
+import { UserListComponent } from 'src/components/user-list/user-list.component';
 
 const routes : Routes = [
     {path: 'about', component: AboutComponent },
+    {path: 'users', component: UserListComponent},
+    {path: 'user-details', component: UserDetailComponent}
 ]
 
 @NgModule ({
@@ -12,3 +16,8 @@ const routes : Routes = [
 })
 
 export class AppRoutingModule {}
+
+export const routingComponents = [ UserListComponent, UserDetailComponent]
+
+
+
